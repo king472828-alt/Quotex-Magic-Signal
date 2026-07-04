0070c39b723b9343817d7f7fbb5e196639bfed0d
king472828-alt <king472828@gmail.com>
2026-07-04 21:19:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # config.py
 
 # Telegram Bot Settings
-TELEGRAM_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"  # Replace with your actual copied token
+TELEGRAM_TOKEN = " 8954190821:AAGdoQsiOlieJoW2Saq-FBm3LTBlSy629eg" # Replace with your actual copied token
 
 # MT5 Login Credentials
-MT5_LOGIN = 12345678  # Replace with your MT5 account number
-MT5_PASSWORD = "your_password"  # Replace with your MT5 password
-MT5_SERVER = "your_broker_server_name"  # Replace with your MT5 server name
+MT5_LOGIN = 1301733467  # Replace with your MT5 account number
+MT5_PASSWORD = "Eemon1234@#%"  # Replace with your MT5 password
+MT5_SERVER = "XMGlobal-MT5 6"  # Replace with your MT5 server name
 
 # 24 Forex Currency Pairs
 FOREX_PAIRS = [
